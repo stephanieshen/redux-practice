fbdafabd7d012ece96260f23fac04825d2a0633a
Shen Sabado <stephanieshen.sabado@gmail.com>
2021-06-22 16:57:53 +0800

    wip manage projects

--- src/pages/ManageProjects/ManageProjects.tsx
+++ src/pages/ManageProjects/ManageProjects.tsx
@@ -1,5 +1,30 @@
+import Button from "../../components/Button/Button";
+import ProjectsList from "../../components/ProjectsList/ProjectsList"
+import Search from "../../components/Search/Search";
+
+import { useHistory } from "react-router-dom";
+
+import styles from './ManageProjects.module.scss';
+
 const ManageProjects = () => {
-    return <p>Manage projects!</p>
+    const history = useHistory();
+
+    const addProject = (): void => {
+        history.push('/manage-projects/add');
+    }
+
+    return (
+        <div className={styles.manageProjects}>
+            <div className={styles.header}>
+                <h3>Hello, Shen!</h3>
+                <Search />
+            </div>
+            <Button type="button" handleClick={addProject}>
+                Add New Project
+            </Button>
+            <ProjectsList />
+        </div>
+    )
 }
 
 export default ManageProjects;
