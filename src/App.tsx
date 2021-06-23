35dad65cfe47e70f12125f81ccec80e8bbb76849
Shen Sabado <stephanieshen.sabado@gmail.com>
2021-06-23 20:18:19 +0800

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import Layout from './components/Layout/Layout';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-function App() {
+const App = () => {
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      },
+      secondary: {
+        main: '#48A09B',
+      }
+    },
+  });
+
   return (
     <div className="App">
-      <Layout></Layout>
+      <ThemeProvider theme={theme}>
+        <Layout></Layout>
+      </ThemeProvider>
     </div>
   );
 }
